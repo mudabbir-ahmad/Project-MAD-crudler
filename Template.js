31c2208c2e4fe5cccc15cd80954ba6ee6d2cfa0e
mudabbir-ahmad <mudabbira.uk@gmail.com>
2025-12-03 20:50:36 +0000

    Added template.js file, makes it easier to create new components

---
+++ Template.js
@@ -0,0 +1,17 @@
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const Component = () => {
+
+    //   Initialisation -------------
+    //   State ----------------------
+    //   Handlers -------------------
+    //   View -----------------------
+
+    return (
+        ;
+);
+};
+
+const styles = StyleSheet.create({});
+
+export default Component;
